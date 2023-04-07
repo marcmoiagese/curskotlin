9ce8d6c5361044b22e15fbba1cd5dd9f7ce3ad2f
Marc Moya Gese <hevercking@hotmail.com>
2023-04-07 21:27:36 +0200

    Definiu tres variables immutables i assigneu-hi els pesos de tres persones amb valors Float. Calculeu la mitjana dels pesos de les persones i mostreu-la.

---
+++ 2-Tipus_de_variables/prob2/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+/**
+ * Definiu tres variables immutables i assigneu-hi els pesos de tres persones amb valors Float. Calculeu la mitjana dels pesos de les persones i mostreu-la.
+ */
+
+fun main(){
+ val ramon = 60.9f
+ val lluis = 78.2f
+ val marc = 58.3f
+
+ var mitjana = ( ramon + lluis + marc )/3
+ println("$mitjana")
+
+}
