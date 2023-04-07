312ec2384a3a3a355eac7a0869e5176fd8a2b5f5
Marc Moya Gese <hevercking@hotmail.com>
2023-04-07 19:25:36 +0200

    optimitzan codi

---
+++ 2-Tipus_de_variables/exemple2/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+/**
+ *  Segons la documentació de Kotlin, una de les seves premisses és que un programa en Kotlin sigui concís, és a dir, que s'expressi de la manera més breu possible.
+ *  Així que farem unes modificacions al codi de l'exemple 1 perquè sigui més breu.
+ */
+
+/**
+fun main(parametre: Array<String>) {
+    val val1: Int = 100
+    val val2: Int = 400
+    var resultat: Int = val1 + val2
+    println("La suma de $val1 + $val2 es $resultat")
+    resultat = val1 * val2
+    println("El producte de $val1 * $val2 es $resultat")
+}
+ */
+
+/**
+ * Un altre exemple per ser més concís és no indicar el tipus de la variable i fer que el compilador de Kotlin el dedueixi.
+ */
+
+fun main(parametre: Array<String>) {
+    val val1 = 100
+    val val2 = 400
+    var resultat = val1 + val2
+    println("La suma de $val1 + $val2 es $resultat")
+    resultat = val1 * val2
+    println("El producte de $val1 * $val2 es $resultat")
+}
