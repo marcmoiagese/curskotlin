8a219f1a05f6e528c54ffa8a83aa41c7d1943046
Marc Moya Gese <hevercking@hotmail.com>
2023-04-13 18:52:30 +0200

    Carregar per teclat tres numeros i mostrar per pantalla el mes gran

--- 6-Estructures_condicionals_aniuades/Exemple3/src/main/kotlin/Main.kt
+++ 6-Estructures_condicionals_aniuades/Exemple3/src/main/kotlin/Main.kt
@@ -11,7 +11,14 @@ fun main(args: Array<String>) {
     println("Dona'm el tercer numero: ")
     val num3 = readln().toInt()
 
-    val gran = if ( num1 > num2 && num1 > num3 ) num1 else if (num1 < num2 && num2 > num3) num2 else num3
-    println("i el numero mes gran es (⌐■_■) $gran")
-
+    if (num1 > num2)
+        if (num1 > num3)
+            print(num1)
+        else
+            print(num3)
+    else
+        if (num2 > num3)
+            print(num2)
+        else
+            print(num3)
 }
