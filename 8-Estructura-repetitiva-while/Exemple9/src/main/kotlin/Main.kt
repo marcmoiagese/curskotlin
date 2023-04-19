3b3bc8971c0f7a6a42d1d01943cca882516aab98
Marc Moya Gese <hevercking@hotmail.com>
2023-04-19 21:20:19 +0200

    Imprimeix per pantalla multiples de 8 fins a 500, tens que mostrar per pantalla 8, 16, 24, etc

---
+++ 8-Estructura-repetitiva-while/Exemple9/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * Imprimeix per pantalla multiples de 8 fins a 500, tens que mostrar per pantalla 8, 16, 24, etc
+ */
+fun main(args: Array<String>) {
+    var vuitM =  8
+
+    while(vuitM <= 500){
+        println(vuitM)
+        vuitM += 8
+    }
+}
