fb428563076cbcc0ad7c2aa2843905cbeac6c822
Marc Moya Gese <hevercking@hotmail.com>
2023-04-19 10:54:27 +0200

    Escriu un programa que permeti carregar 10 valors a través del teclat i que ens imprimeixi la suma i el promig dels valors introduïts.

---
+++ 8-Estructura-repetitiva-while/Exemple3/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+/**
+ * Escriu un programa que permeti carregar 10 valors a través del teclat i que ens imprimeixi la suma i el promig dels valors introduïts.
+ */
+
+fun main(args: Array<String>) {
+    var x = 1
+    var suma = 0
+
+    while(x<=10){
+        println("Donam el numero $x : ")
+        val numnum = readln().toInt()
+        suma = suma + numnum
+        x++
+    }
+    val promig = suma/10
+    println("La suma dels numeros es $suma i el seu promig es $promig")
+}
