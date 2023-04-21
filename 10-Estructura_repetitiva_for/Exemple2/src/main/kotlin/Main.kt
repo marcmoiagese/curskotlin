419c19393056d31200f92f7a825884e28138f791
Marc Moya Gese <hevercking@hotmail.com>
2023-04-21 23:14:13 +0200

    Fes un programa que permeti la introducció de 10 valors per teclat i mostri posteriorment la suma dels valors introduïts i la seva mitjana. Aquest problema ja l'hem resolt anteriorment quan explicàvem l'estructura while, però en aquest cas el resoldrem utilitzant l'estructura 'for'.

---
+++ 10-Estructura_repetitiva_for/Exemple2/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+/**
+ * Fes un programa que permeti la introducció de 10 valors per teclat i mostri posteriorment la suma dels valors introduïts i la seva mitjana.
+ * Aquest problema ja l'hem resolt anteriorment quan explicàvem l'estructura "while", però en aquest cas el resoldrem utilitzant l'estructura "for".
+ */
+
+
+fun main(args: Array<String>) {
+    var suma = 0
+    for(i in 1..10){
+       println("Donam un numero: ")
+       val numero = readln().toInt()
+       suma  = suma + numero
+    }
+    val mitjana = suma /10
+    println("La suma es $suma i la mitjana es $mitjana")
+}
