ae853cac95ffded6960034f9c70e44ae7df62728
Marc Moya Gese <hevercking@hotmail.com>
2023-04-21 23:04:28 +0200

    Realitza un programa que imprimeixi per pantalla els numeros del 1 al 100

--- 10-Estructura_repetitiva_for/Exemple1/src/main/kotlin/Main.kt
+++ 10-Estructura_repetitiva_for/Exemple1/src/main/kotlin/Main.kt
@@ -1,20 +1,13 @@
 /**
- * For a Kotlin te algunes variants, en aquest cas veurem for amb expresions de rang.
- * Un rang defineix un interval amb un valor inicial i un vlor final. i es defineix utilitzan el valor ..
- * Exemples de definició de rang serien:
- * val undigit = 1..9
- * val dotzena = 1..12
- * var lletres = 'a'..'z'
- *
- * Si nesecitem coneixer si un valor es troba dins un rang hem d'utilitzar l'operador in o !in
- *
- *
+ * Realitza un programa que imprimeixi per pantalla els numeros del 1 al 100
  */
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    for (i in 1..100)
+        println(i)
 }
+
+/**
+ * La variable 'i' es defineix de tipus 'Int' per inferència, ja que el rang és de 1..100.
+ * A la primera execució del bucle, la variable 'i' obté el valor 1, a la segona el 2 i així successivament.
+ */
