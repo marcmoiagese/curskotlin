64ce13c0d7d3aef308810dbb8f56c4464ceb2f7e
Marc Moya Gese <hevercking@hotmail.com>
2023-04-23 15:53:23 +0200

    Condicional if

--- 4-Condicional_if/Exemple1/src/main/kotlin/Main.kt
+++ 4-Condicional_if/Exemple1/src/main/kotlin/Main.kt
@@ -1,9 +1,4 @@
 /**
- * Quan ens trobem davant de la presa de decisions, disposem de les estructures condicionals per ajudar-nos a triar.
- * Haig d'optar per uns estudis o per uns altres? Prefereixo menjar pollastre o sopa? He de decidir entre seguir el camí A o el camí B, entre altres opcions.
- *
- * Estructura condicional simple
- * Una estructura condicional simple és quan, enfrontats a una decisió, prenem una única opció en funció d'una condició específica.
  *
  * Per exemple, donat el salari d'una persona i aquest supera els 1500€, mostrarem un missatge per pantalla indicant que ha de pagar impostos.
  *
---
+++ 4-Condicional_if/README.md
@@ -0,0 +1,7 @@
+# Condicional IF
+
+Quan ens trobem davant de la presa de decisions, disposem de les estructures condicionals per ajudar-nos a triar. Haig d'optar per uns estudis o per uns altres? Prefereixo menjar pollastre o sopa? He de decidir entre seguir el camí A o el camí B, entre altres opcions. 
+
+## Estructura condicional simple
+
+Una estructura condicional simple és quan, enfrontats a una decisió, prenem una única opció en funció d'una condició específica.
