1e09930921eaf66b3117beea1731e8556175cdc0
Marc Moya Gese <hevercking@hotmail.com>
2023-06-14 20:50:32 +0200

    Carrega un vector de 10 elements de tipus enter i verifica posteriorment si el mateix esta ordenat de menor a major.

---
+++ 20-Vectors_conceptes/Exemple3/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+/**
+ * Carrega un vector de 10 elements de tipus enter i verifica posteriorment si el mateix esta ordenat de menor a major
+ */
+
+fun main(args: Array<String>) {
+    val vector = IntArray(10)
+    for(i in 0..vector.size-1){
+        print("donam un numero: ")
+        vector[i] = readln().toInt()
+    }
+
+    var ordenat = true
+    for (i in 0..vector.size-2)
+        if(vector[i+1]<vector[i])
+            ordenat = false
+
+    if(ordenat)
+        print("Els elements estan ordenats de mes petit a mes gran")
+    else
+        print("Els elements no estan ordenats de mes petit a mes gran")
+}
