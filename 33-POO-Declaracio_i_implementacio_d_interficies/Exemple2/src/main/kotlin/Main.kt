7a5b5b8760f869c96ba9620e5ab3e665b332806b
Marc Moya Gese <hevercking@hotmail.com>
2023-06-26 23:11:19 +0200

    Exemple 2

---
+++ 33-POO-Declaracio_i_implementacio_d_interficies/Exemple2/src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+import java.awt.Rectangle
+
+/**
+ * Donada la seguent interficie, Definirem dues classes que representin un cuadrat i un rectangle. Implementarem la interficie figura a ambdues classes
+ */
+
+interface Figura {
+    fun calculaSuperficie(): Int
+    fun calcularPerimetre(): Int
+    fun titolResultat() {
+        println("Dades de la figura")
+    }
+}
+
+class Cuadrat ( val costat: Int): Figura {
+    override fun calculaSuperficie():Int {
+        return costat * costat
+    }
+
+    override fun calcularPerimetre():Int {
+        return costat * 4
+    }
+}
+
+class RectAngle (val costatGran: Int, val menor: Int ): Figura {
+    override fun calculaSuperficie(): Int {
+        return costatGran * menor
+    }
+
+    override fun calcularPerimetre():Int {
+        return (costatGran * 2) + (menor * 2)
+    }
+}
+
+fun main(args: Array<String>) {
+    val  cuadrat1 = Cuadrat(10)
+    cuadrat1.titolResultat()
+    println("Perimetre del cuadrat: ${cuadrat1.calcularPerimetre()}")
+    println("Superficie del cuadrat : ${cuadrat1.calculaSuperficie()}")
+
+    val rectangle1 = RectAngle(10,5)
+    rectangle1.titolResultat()
+    println("Perimetre del rectangle: ${rectangle1.calcularPerimetre()}")
+    println("Superficie del rectangle : ${rectangle1.calculaSuperficie()}")
+
+}
