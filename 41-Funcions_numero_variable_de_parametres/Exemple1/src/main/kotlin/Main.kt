be3af256b7b16a4bb90b855ca7cf4e4d7e21b9dd
Marc Moya Gese <hevercking@hotmail.com>
2023-07-04 15:45:18 +0200

    Exemple 1

--- 41-Funcions_numero_variable_de_parametres/Exemple1/src/main/kotlin/Main.kt
+++ 41-Funcions_numero_variable_de_parametres/Exemple1/src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@
  * Farem una funció que rebi una cantitat variable d'enters i ens retorni la seva suma
  */
 
+// La funcio sumar rep un parametre amb un numero variable d'elements
 fun sumar(vararg numeros: Int): Int{
+    //Dins recorrem el vector i sumem els seus elements
     var suma = 0
     for (element in numeros)
         suma += element
@@ -10,6 +12,8 @@ fun sumar(vararg numeros: Int): Int{
 }
 
 fun main(args: Array<String>) {
+
+    // Cridem la funcio sumar enviant-li els X elements
     val total = sumar(10, 20, 30, 40, 50)
     println(total)
 }
