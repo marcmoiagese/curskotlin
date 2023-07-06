cb3d19ab307a886b1638e9b9adebd76a7018d68b
Marc Moya Gese <hevercking@hotmail.com>
2023-07-06 20:50:07 +0200

    Problema1

--- 41-Funcions_numero_variable_de_parametres/Problema1/src/main/kotlin/Main.kt
+++ 41-Funcions_numero_variable_de_parametres/Problema1/src/main/kotlin/Main.kt
@@ -2,15 +2,6 @@
  * Fes una funció que rebi una serie d'edats i ens retorni la cantitat que son mes grans o iguals a 18 (com a minim s'envia un enter a la funció)
  */
 
-fun major18 (vararg edats: Int): Int{
-    var cnt = 0
-    for (element in edats)
-        if(element >= 18)
-            cnt++
-    return cnt
-}
+fun major18(vararg edats: Int) = edats.count {it >=18}
 
-fun main(args: Array<String>) {
-    val envioEdats = major18(5, 10, 20, 60, 5)
-    println(envioEdats)
-}
+fun main(args: Array<String>) = println(major18(5, 10, 20, 60, 5))
