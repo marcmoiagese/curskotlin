2c75c2c0673d7bd539c8c2298e110011c0d260a9
Marc Moya Gese <hevercking@hotmail.com>
2023-07-06 20:31:48 +0200

    Exemple 2

--- 41-Funcions_numero_variable_de_parametres/Exemple2/src/main/kotlin/Main.kt
+++ 41-Funcions_numero_variable_de_parametres/Exemple2/src/main/kotlin/Main.kt
@@ -2,11 +2,13 @@
  * Realitzarem una funció que rebi com a primer paràmetre, el tipus d'operacio que vulguem realitzar ambe amb les seguents dades enters que li enviem
  */
 
+// Declarem la funció amb dos valors possibles
 enum class Operacio {
     SUMA,
     PROMIG
 }
 
+// A la  funció Operar li enviem com a parametre un tupis de dada Operació i com a segon parametre sera la llista de parametres a processar
 fun operar(tipusOperacio: Operacio, vararg vector: Int): Int{
     when (tipusOperacio){
         Operacio.SUMA -> return vector.sum()
@@ -15,9 +17,11 @@ fun operar(tipusOperacio: Operacio, vararg vector: Int): Int{
 }
 
 fun main(args: Array<String>) {
+    // Cuan crirem operar, com a primera dada el tipus d'operacio que volem fer seguit de la llista d'enters
     val resultat = operar(Operacio.SUMA, 10, 20, 30)
     println("La suma es $resultat")
 
+    // Cuan crirem operar, com a primera dada el tipus d'operacio que volem fer seguit de la llista d'enters
     val resultat2 = operar(Operacio.PROMIG, 10, 20, 30)
     println("El promig es $resultat2")
 }
