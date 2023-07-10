f45189458f6766aa7c1870c4e893de32a1c30481
Marc Moya Gese <hevercking@hotmail.com>
2023-07-10 18:53:45 +0200

    Problema 1

---
+++ 47-Package_i_Import/Problema1/src/main/kotlin/Principal.kt
@@ -0,0 +1,7 @@
+import entradadeteclat.*
+
+fun main(args: Array<String>) {
+    val donamUnint = retornarInt("Donam el primer valor: ")
+    val donamunatltreInt = retornarInt("Donam el segon valor: ")
+    println("la suma de $donamUnint i $donamunatltreInt es ${donamUnint + donamunatltreInt}")
+}
---
+++ 47-Package_i_Import/Problema1/src/main/kotlin/entradadeteclat/entrada.kt
@@ -0,0 +1,16 @@
+package entradadeteclat
+
+fun retornarInt(missatge: String): Int{
+    print(missatge)
+    return readln().toInt()
+}
+
+fun retornarDouble(missatge: String): Double {
+    print(missatge)
+    return readln().toDouble()
+}
+
+fun retornarFloat(missatge: String): Float{
+    print(missatge)
+    return readln().toFloat()
+}
