64c68cb97e8760c71c38b5c379f5376ba8800e7a
Marc Moya Gese <hevercking@hotmail.com>
2023-08-02 21:37:46 +0200

    Afegint exercici 3

--- Kotlin_per_Android/3-Capturar_el_clic_dun_boto/Exemple1/app/src/main/java/com/mdemarc/exemple1/MainActivity.kt
+++ Kotlin_per_Android/3-Capturar_el_clic_dun_boto/Exemple1/app/src/main/java/com/mdemarc/exemple1/MainActivity.kt
@@ -12,6 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val et1=findViewById<EditText>(R.id.et1)
         val et2=findViewById<EditText>(R.id.et2)
+        val tv1=findViewById<TextView>(R.id.tv1)
+        val button=findViewById<TextView>(R.id.button)
+        button.setOnClickListener{
+            val et1=findViewById<EditText>(R.id.et1)
+            val nro1 = et1.text.toString().toInt()
+            val nro2 = et2.text.toString().toInt()
+            val suma = nro1 + nro2
+            tv1.text = "Resultat: ${suma.toString()}"
+        }
         
     }
 }
